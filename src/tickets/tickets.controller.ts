02c134154414fd0a1638e1c30c974763fd1f6829
Sabbir Ahmed <prince534534@gmail.com>
2026-09-11 18:50:49 +0600

    added logger middleware

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { TicketsService } from './tickets.service.js';
 import { CreateTicketDto } from './dto/create-ticket.dto.js';
 import { FilterTicketsQueryDto } from './dto/filter-tickets-query.dto.js';
+import { UpdateTicketDto } from './dto/update-ticket.dto.js';
 
 @Controller('tickets')
 export class TicketsController {
@@ -18,5 +19,18 @@ export class TicketsController {
     @Post()
     create(@Body() createTicketDto: CreateTicketDto) {
         return this.ticketsService.create(createTicketDto)
+    }
+
+    @Patch(':id')
+    update(
+        @Param('id', ParseIntPipe) id:number,
+        @Body() updateTicketDto: UpdateTicketDto,
+    ){
+        return this.ticketsService.update(id, updateTicketDto)
+    }
+
+    @Patch(':id/close')
+    closeTicket(@Param('id', ParseIntPipe) id:number) {
+        return this.ticketsService.closeTicket(id)
     }
 }
